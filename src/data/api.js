03e1035b77dd5eb772b28add3e9a17365630359f
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-06-09 09:58:50 +0200

    Added temporary api

---
+++ src/data/api.js
@@ -0,0 +1,20 @@
+export const api = [
+  {
+    title: "Termometerfabrikken Blodtrykksmåler",
+    stars: 2,
+    price: "499 ,-",
+    stock: 25,
+  },
+  {
+    title: "Beurer blodtrykksmåler med ECG BM 95",
+    stars: 2,
+    price: "1569 ,-",
+    stock: 5,
+  },
+  {
+    title: "Withings Core smart blodtrykksmåler WITBPM550067",
+    stars: 4,
+    price: "2490 ,-",
+    stock: 5,
+  },
+];
