ef00ba5a6563842fda1259eb6728cfefc89a8d65
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-06-10 11:29:08 +0200

    added columns and form on product detail page

--- src/components/product/ProductPage.js
+++ src/components/product/ProductPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { api } from "../data/api";
 import MainHeading from "../layout/headings/MainHeading";
@@ -8,10 +9,28 @@ function ProductPage() {
   const { id } = useParams("/");
 
   return (
-    <div>
-      <p>All &gt; Blodtrykksmåler</p>
-      <MainHeading title={api[id].title} />
-    </div>
+    <Row>
+      <Col xs md={7}>
+        <p>All &gt; Blodtrykksmåler</p>
+        <MainHeading title={api[id].title} />
+        <img src={api[id].img} className="w-100" />
+      </Col>
+      <Col xs md={5}>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>(a) General description of the device</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>(b) UDI-DI/UDI-PI</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+        </Form>
+      </Col>
+    </Row>
   );
 }
 
